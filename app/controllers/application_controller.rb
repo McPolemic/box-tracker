a83094b5125ed57d4884396b221ff6d8c39367ec
August Lukens <august@lukens.co>
2025-11-13 20:07:56 -0500

    Hide write-access features from unauthenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :write_access?
+
+  private
+
+  def write_access?
+    cookies[:write_access] == "granted"
+  end
 end
